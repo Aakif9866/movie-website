8b27f91ee150b859c3efdb094d243420fce865fd
Aakif9866 <aakif9866@gmail.com>
2024-06-15 14:14:21 +0530

    handle error added

---
+++ src/app/error.jsx
@@ -0,0 +1,20 @@
+// works due to next js properties
+"use client";
+
+import { useEffect } from "react";
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <div className="text-center mt-10">
+      <h1>Something went wrong. Please try again later.</h1>
+      <button
+        className=" bg-red-300 mt-5 hover:text-amber-600"
+        onClick={() => reset()}
+      >
+        Try Again
+      </button>
+    </div>
+  );
+}
