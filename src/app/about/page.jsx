4a367bfddb587fcce2b16dade6b6966c1073bddf
Aakif9866 <aakif9866@gmail.com>
2024-06-15 16:31:10 +0530

    some changes made

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function page() {
+export default function Aboutpage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-2xl mx-auto p-3 text-center">
         <div>
-          <h1 className="text-3xl font font-semibold text-center my-7">
+          <h1 className="text-3xl font-semibold text-center my-7">
             About IMDB CLONE
           </h1>
           <div className="text-md text-gray-500 flex flex-col gap-6">
@@ -17,7 +17,6 @@ export default function page() {
             </p>
 
             <p>
-              {" "}
               🔍 Explore: Dive into a diverse range of topics and expand your
               horizons.
             </p>
@@ -39,21 +38,18 @@ export default function page() {
                 href="https://github.com/Aakif9866/movie-website"
                 className="text-blue-600 hover:underline hover:text-blue-800"
               >
-                {" "}
                 GitHub
               </a>{" "}
               repository to learn more about the project.
             </p>
 
             <p>
-              {" "}
               Connect with us on{" "}
               <a
                 target="_blank"
                 href="https://github.com/Aakif9866"
                 className="text-blue-600 hover:underline hover:text-blue-800"
               >
-                {" "}
                 GitHub
               </a>{" "}
               and be part of our journey!
